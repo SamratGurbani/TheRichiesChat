f495805fce5adb968f0d8d490135de9cff041ee4
Mohit Gurbani <37788491+SamratGurbani@users.noreply.github.com>
2019-03-31 04:44:44 +0530

    Seventh Part, Friend list option

--- app/src/main/java/com/samrat/therichieschat/MainActivity.java
+++ app/src/main/java/com/samrat/therichieschat/MainActivity.java
@@ -124,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
             RequestNewGroup();
         }
         if (item.getItemId() == R.id.main_find_friends_option){
-
+            SendUserToFindFriendsActivity();
         }
         return true;
     }
@@ -181,6 +181,11 @@ public class MainActivity extends AppCompatActivity {
         settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(settingsIntent);
         finish();
+    }
+
+    private void SendUserToFindFriendsActivity() {
+        Intent findFriendsIntent = new Intent(MainActivity.this,FindFriendsActivity.class);
+        startActivity(findFriendsIntent);
     }
 
 }
